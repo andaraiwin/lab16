3730ed4a26f77af31d3c1a634d1c35042b84242c
Raiwin Inthasit <andaraiwin@gmail.com>
2022-01-14 18:55:15 +0700

    Completed

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -1,9 +1,25 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
+	int a = 5;
+	char b = 'A';
+	char &c = b;
+	void *x = &a;
+	void *y = &b;
+	void *z = x;
 
-	
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << "\n";
+	cout << &a << " " << (void*)&b << " " << (void*)&c << " " << &x << " " << &y << " " << &z << "\n";
+	b = 'F';
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << "\n";
+	b = 'W';
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << "\n";
+	*(int*)x = 6;
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << "\n";
+	*(int*)z = 7;
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << "\n";
 	
 	return 0;
 }
